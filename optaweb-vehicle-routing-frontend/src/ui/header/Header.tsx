eb2ee9aec8ccf1238a95c905e4ecaad029af9ca9
Jiří Locker <jiri.locker@gmail.com>
2020-07-02 21:24:55 +0200

    WIP

--- optaweb-vehicle-routing-frontend/src/ui/header/Header.tsx
+++ optaweb-vehicle-routing-frontend/src/ui/header/Header.tsx
@@ -21,7 +21,8 @@ import Toolbar from './Toolbar';
 
 const Header: React.FC = () => (
   <PageHeader
-    logo={<Brand src="./assets/images/optaPlannerLogo200px.png" alt="OptaPlanner Logo" />}
+    // logo={<Brand src="./assets/images/optaPlannerLogo200px.png" alt="OptaPlanner Logo" />}
+    logo={<Brand src="./assets/images/optaplanner_logo_dk_200px.png" alt="OptaPlanner Logo" />}
     toolbar={<Toolbar />}
     topNav={<Navigation />}
   />
